77fb3ccb441d6fdb1811b9e2af49ecffb62fdac0
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2025-07-28 16:23:13 +0530

    add code to see overflow of floats

---
+++ code/ch02/floats_sum_overflow.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "show_bytes.c"
+
+int main()
+{
+    // Declaring the max normalized 32 bit float
+    union
+    {
+        int u;
+        float f;
+    } max_float;
+    max_float.u = 0x7F7FFFFF;
+
+    // Initializing the max normalized 32 bit float as f1 and f2
+    float f1 = max_float.f;
+    float f2 = max_float.f;
+
+    // Printing the value of f1 and f2 and the binary representation
+    printf("f1: %f\n", f1);
+    show_bytes_in_binary((unsigned char *)&f1, sizeof(f1));
+    printf("f2: %f\n", f2);
+    show_bytes_in_binary((unsigned char *)&f2, sizeof(f2));
+
+    // Computing the sum of f1 and f2
+    float f3 = f1 + f2;
+    printf("f1 + f2: %f\n", f3);
+    show_bytes_in_binary((unsigned char *)&f3, sizeof(f3));
+
+    return 0;
+}
